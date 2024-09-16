db74a6c52d64062c73b2d5955cb33742c5e51fc5
DR33Y <74345638+DR33Y@users.noreply.github.com>
2024-09-16 09:01:34 +0300

    Update ArraysPractice.java

--- ArraysPractice.java
+++ ArraysPractice.java
@@ -45,14 +45,70 @@ public class Tsester {
 		int i, sum=0;
 		
 		for(i=0; i<arr.length; i++) {
-			if(arr[i].compareTo(s) == 0)
+			if(arr[i].equals(s))
 				sum += 1;
 		}
 		return sum;
 	}
+	public static boolean f9(int[] arr) {
+		int i;
+		
+		for(i=0; i<arr.length-2; i++) {
+			if(arr[i] + arr[i+1] != arr[i+2])
+				return false;
+		}
+		return true;
+	}
+	public static int f10(int[] arr) {
+		int i,j;
+		int num=0;
+		
+		for(i=0; i<arr.length; i++) {
+			int apper=0         ;
+			for(j=0; j<arr.length; j++) {
+				if(arr[i] == arr[j])
+					apper++;
+			}
+			if(apper==2)
+				num = arr[i];
+		}
+		return num;
+	}
+	public static void f11(int[] arr, int x) {
+		int i,j;
+		
+		for(i=0; i<arr.length; i++) {
+			for(j=0; j<arr.length; j++) {
+				if(arr[i]+arr[j] == x)
+					System.out.println(arr[i]+","+arr[j]);
+			}
+		}
+	}
+	public static int[] f12(int[] arr, int n, int m) {
+		int[] newArray;
+		int i,cnt=0;
+		
+		for(i=0; i<arr.length; i++) {
+			if(arr[i]>n && arr[i]<m)
+				cnt++;
+		}
+		newArray = new int[cnt];
+		
+		int index=0;
+		for(i=0; i<arr.length; i++) {
+			if(arr[i]>n && arr[i]<m) {
+				newArray[index] = arr[i];
+				index++;
+			}	
+		}
+		return newArray;
+	}
+	public static char[] f13(String[] arr) {
+		int i
+	}
 	public static void main(String[] args) {
-		String[] arr = {"ana", "bara", "amalia"};
-		System.out.println(f4(arr));
+		int[] arr = {2,4,3,2,1,4,4};
+		System.out.println(f10(arr));
 	}
 }
      
